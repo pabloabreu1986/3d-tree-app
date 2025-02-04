6fa68d076a4bf1c52bf2c8b35fee200f3cdc6af4
pabloabreu1986 <pablo.abreu1986@gmail.com>
2025-02-04 15:25:21 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
  server: {
-		allowedHosts: ['a842-2a0c-5a81-e110-6000-2195-b92c-4fb6-e7a3.ngrok-free.app']
+		allowedHosts: ['external-shared-host']
 	}
 })
 
